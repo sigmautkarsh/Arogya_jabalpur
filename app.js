f9568f18e45565bfb8c46194b1c78f44f34d233b
Mohit132 <mohitdubey1322001@gmil.com>
2023-05-09 02:07:17 -0700

    9/05/2023  remove api folder

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(cors({
     origin: '*'
 }));
 const cookieParser =require('cookie-parser')
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
